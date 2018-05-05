e8052dfe65c4e5750946a62b97a36247b8a46986
jiaowagn <wangjiaohere@gmail.com>
2018-05-05 14:40:30 +0800

    Admin can access and change user emails

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ Rails.application.routes.draw do
   namespace :admin do
     root "events#index"
     resources :events
+    resources :users 
   end
 
   get "/faq" => "pages#faq"
 
   # 这里的路由设计使用单数 resource:user，跟 resources :users 相比，单数的路由少了 index action，并且网址上不会有 ID，
   # 路由方法也皆为单数不需要参数，例如 user_path、edit_user_path
-  resource :user 
+  resource :user
 
   root "events#index"
 
